f967aa0823abd72e019bae3b63392afc2f436702
Choon Wee <142023227+CWHengProj@users.noreply.github.com>
2024-11-18 11:07:12 +0800

    Initial commit

---
+++ src/test/java/com/ws12/SSFws12/SsfWorkshop12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ws12.SSFws12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfWorkshop12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
